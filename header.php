70674f68e8eada18703d6896c870897cde9c4b9d
Arul R <arul.ramu04@gmail.com>
2023-03-24 11:47:48 +0530

    Create header.php
    Header page created

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sample page</title>
+    <meta charset="utf-8">
+    <meta name="description" description="">
+    <link rel="stylesheet" href="style.css">
+  </head>

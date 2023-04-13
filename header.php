6a815437d37e79a5330a54feec4672df016a7109
arulramu <arul.ramu04@gmail.com>
2023-04-13 16:58:12 +0530

    title name changed

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Sample page</title>
+    <title>Sample page 2</title>
     <meta charset="utf-8">
     <meta name="description" content="Sample">
     <link rel="stylesheet" href="style.css">

713c37fbdd32f515e05fa7da14000163fd1cf135
Arul R <arul.ramu04@gmail.com>
2023-04-15 14:41:27 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Sample page 2</title>
+    <title>Sample page of php</title>
     <meta charset="utf-8">
     <meta name="description" content="Sample">
     <link rel="stylesheet" href="style.css">

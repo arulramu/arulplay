11e968c9d49289dbe048ca65602e62fccbbc3d7a
Arul R <arul.ramu04@gmail.com>
2024-06-03 23:02:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "header.php";
 
   <h1>Home Page11</h1>
   <p>This interaction between github and localmechine Running using visual studio</p>
-  <p>jenkins</p>
+  <p>jenkins </p>
   <p>2023-2024</p>
 
  

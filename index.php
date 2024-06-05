2d14857cc331b0254770dbc2e268d21cea40ac36
Arul R <arul.ramu04@gmail.com>
2024-06-05 15:54:10 +0530

    Heading mofified

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 include "header.php";
 ?>
 <body>
-<h2>find the sollution for 302</h2>
+<h2>Welcome micro service pipe line</h2>
   <h1>Home Page</h1>
   <p>This interaction between github and localmechine Running using visual studio</p>
   <p>jenkins </p>
   <p>2023-2024</p>
   <p>Learning </p>
+<p>
 
 </body>
 <?php

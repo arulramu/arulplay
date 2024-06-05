8d9eca8dbac57fdeb571bb553e9d29122ad53587
Arul R <arul.ramu04@gmail.com>
2024-06-05 16:09:36 +0530

    add name murali

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include "header.php";
 ?>
 <body>
 <h2>Welcome micro service pipe line</h2>
+<p>Murali krishnan</p>
   <h1>Home Page</h1>
   <p>This interaction between github and localmechine Running using visual studio</p>
   <p>jenkins </p>
